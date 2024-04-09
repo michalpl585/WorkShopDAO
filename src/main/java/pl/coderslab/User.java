b2ede3f991399a67263499cd1b1b1e73f198aa38
michalpl585 <michalpl585@interia.pl>
2024-04-10 01:04:08 +0200

    michalpl

--- src/main/java/pl/coderslab/User.java
+++ src/main/java/pl/coderslab/User.java
@@ -3,9 +3,11 @@ package pl.coderslab;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class User {
 
     private int id;
